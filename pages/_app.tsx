40dbb6053f46ebdac5255417daf183caccf11dc1
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2025-04-21 17:20:17 +0900

    배포 오류 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from '@/layouts/Layout';
 import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Layout>
 			<Component {...pageProps} />
