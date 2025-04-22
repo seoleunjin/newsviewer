8b318624e29aca74177244af119a9dfc06294e9e
seoleunjin <anah9026@gmail.com>
2025-04-22 10:29:02 +0900

    병합

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import Layout from '@/layouts/Layout';
 import '@/styles/globals.css';
+import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
